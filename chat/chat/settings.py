51cf2dead3d659d9e576b8d7b7da40a45309abfb
alex-njuguna <njugunakinuthia013@gmail.com>
2023-08-08 00:49:01 +0300

    login logiut functionality

--- chat/chat/settings.py
+++ chat/chat/settings.py
@@ -111,3 +111,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# LOGOUT_REDIRECT = '/login/'
+LOGIN_REDIRECT = '/rooms/'
+LOGIN_URL = '/login/'
+
+
+
--- chat/core/urls.py
+++ chat/core/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
+
 from . import views
 
 
 urlpatterns = [
     path("", views.frontpage, name="frontpage"),
     path('signup/', views.signup, name="signup"),
+    path('login/', auth_views.LoginView.as_view(template_name='core/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     
 ]
