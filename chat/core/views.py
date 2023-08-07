aaf69dbf7fe03204900d7374719ec1ab66600221
alex-njuguna <njugunakinuthia013@gmail.com>
2023-08-08 00:19:29 +0300

    base, frontpage and views

--- chat/core/views.py
+++ chat/core/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
 
-# Create your views here.
+from .forms import SignUpForm
+
+
+def frontpage(request):
+    
+    return render(request, 'core/frontpage.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        
+        if form.is_valid():
+            user = form.save()
+            
+            login(request, user)
+            
+            return redirect('frontpage')
+        
+    else:
+        form = SignUpForm()
+    
+    return render(request, 'core/signup.html', {'form':form})
+
