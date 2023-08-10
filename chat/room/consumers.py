0896bea67d325e34cfd73fbe9dc304545ee31396
alex-njuguna <njugunakinuthia013@gmail.com>
2023-08-10 06:29:21 +0300

    storing messages

--- chat/room/consumers.py
+++ chat/room/consumers.py
@@ -3,6 +3,10 @@ import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 from asgiref.sync import sync_to_async
 
+from .models import Message, Room
+
+from django.contrib.auth.models import User
+
 
 class ChatConsumer(AsyncWebsocketConsumer):
 
@@ -29,6 +33,8 @@ class ChatConsumer(AsyncWebsocketConsumer):
         username = data['username']
         room = data['room']
 
+        await self.save_message(username, room, message)
+
         await self.channel_layer.group_send(
             self.room_group_name,
             {
@@ -49,5 +55,12 @@ class ChatConsumer(AsyncWebsocketConsumer):
             'username': username,
             'room': room,
         }))
+
+    @sync_to_async
+    def save_message(self, username, room, message):
+        user = User.objects.get(username=username)
+        room = Room.objects.get(slug=room)
+
+        Message.objects.create(user=user, room=room, content=message)
 
 
