2071ae71231a7c30faad961311594b2dc42beb1b
alex-njuguna <njugunakinuthia013@gmail.com>
2023-08-10 15:30:52 +0300

    routing bug fix

--- chat/core/urls.py
+++ chat/core/urls.py
@@ -7,7 +7,7 @@ from . import views
 urlpatterns = [
     path("", views.frontpage, name="frontpage"),
     path('signup/', views.signup, name="signup"),
-    path('login/', auth_views.LoginView.as_view(template_name='core/login.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='core/login.html', next_page='room:rooms'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     
 ]
