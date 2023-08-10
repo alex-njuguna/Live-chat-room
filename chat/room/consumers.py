311c5fec4f2115356bf187395299c9b344a328f7
alex-njuguna <njugunakinuthia013@gmail.com>
2023-08-10 06:06:17 +0300

    sending messages

--- chat/room/consumers.py
+++ chat/room/consumers.py
@@ -23,3 +23,31 @@ class ChatConsumer(AsyncWebsocketConsumer):
             self.channel_name
         )
 
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        message = data['message']
+        username = data['username']
+        room = data['room']
+
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type':'chat_message',
+                'message': message,
+                'username': username,
+                'room': room,
+            }
+        )
+
+    async def chat_message(self, event):
+        message = event['message']
+        username = event['username']
+        room = event['room']
+
+        await self.send(text_data=json.dumps({
+            'message': message,
+            'username': username,
+            'room': room,
+        }))
+
+
